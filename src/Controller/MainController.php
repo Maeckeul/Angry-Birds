1cd8bed0b4755c9fe351ca80615cfe8ab9f8d221
Maeckeul <cmaeckeul@gmail.com>
2020-08-14 22:56:24 +0200

    :construction: homepage.html.twig created and return with render()

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController {
+class MainController extends AbstractController {
 
     /**
     * @Route("/")
     */
     public function homepage() {
-        $response = new Response('<h1>Homepage</h1>');
+        //$response = new Response('<h1>Homepage</h1>');
 
-        return $response;
+        //return $response;
+
+        return $this->render('homepage.html.twig');
     }
 
     /**
