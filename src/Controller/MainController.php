a28347625d93d6d29ea21fb8aa9a69ab27e41628
Maeckeul <cmaeckeul@gmail.com>
2020-08-14 22:35:14 +0200

    :package: twig installed

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -27,7 +27,7 @@ class MainController {
     }
 
     /**
-     * @Route("/product/{id}", requirements={"id"="\d+"})
+     * @Route("/product/{id}", name="product", requirements={"id"="\d+"})
      */
     public function product(int $id) {
         $response = new Response("<h1>Product $id</h1>");
