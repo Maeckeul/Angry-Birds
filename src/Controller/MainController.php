4b92e39ebd4d3c21317b5e6149896f00680df9b2
Maeckeul <cmaeckeul@gmail.com>
2020-08-14 21:40:26 +0200

    :construction: Function contact and road created

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,4 +11,10 @@ class MainController {
 
         return $response;
     }
+
+    public function contact() {
+        $response = new Response('<h1>Contact</h1>');
+
+        return $response;
+    }
 }
