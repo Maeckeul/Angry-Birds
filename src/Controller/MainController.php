9f7bb9a88a8cb41e6b5e4fc0ad4055c8fb0b1333
Maeckeul <cmaeckeul@gmail.com>
2020-08-14 22:08:13 +0200

    :construction: routing created with annontations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController {
 
+    /**
+    * @Route("/")
+    */
     public function homepage() {
         $response = new Response('<h1>Homepage</h1>');
 
         return $response;
     }
 
+    /**
+    * @Route("/contact")
+    */
     public function contact() {
+        
         $response = new Response('<h1>Contact</h1>');
 
         return $response;
