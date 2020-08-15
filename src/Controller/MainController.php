cf8e7665d3890d2d2c793c3490ed8d8cc18e76a7
Maeckeul <cmaeckeul@gmail.com>
2020-08-15 13:38:57 +0200

    base.html.twig configured, roads homepage and birdDetails created and Bird Model

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,39 +1,31 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use App\Model\Bird;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController {
 
     /**
-    * @Route("/")
-    */
-    public function homepage() {
-        //$response = new Response('<h1>Homepage</h1>');
-
-        //return $response;
-
-        return $this->render('homepage.html.twig', ["date" => "14/08/2020"]);
-    }
-
-    /**
-    * @Route("/contact")
-    */
-    public function contact() {
-        
-        $response = new Response('<h1>Contact</h1>');
-
-        return $response;
-    }
-
-    /**
-     * @Route("/product/{id}", name="product", requirements={"id"="\d+"})
+     * @Route("/", name="homepage")
      */
-    public function product(int $id) {
-        $response = new Response("<h1>Product $id</h1>");
-        return $response;
+    public function homepage() {
+
+        $birdModel = new Bird();
+        $birds = $birdModel->getBirds();
+
+        return $this->render('homepage.html.twig', [
+            "birds" => $birds
+        ]);
+    }
+
+    /**
+     * @Route("/bird/{id}", name="bird_detail")
+     */
+    public function birdDetail(int $id) {
+
+        return $this->render('homepage.html.twig', []);
     }
 }
